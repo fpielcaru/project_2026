9ded14dcfcb0946bbfd365259742dcc06531796d
fpielcaru <fpielcaru@gmail.com>
2026-01-04 16:33:27 +0200

    Iteration

---
+++ new_project2026.py/iteration_in_disctionary.py
@@ -0,0 +1,11 @@
+hospital = {
+    "departments" : "cardiology", 
+    "machines" : "CT Scanner",
+    "doctors" : 25, 
+    "nurses" : 40, 
+    "company" : "MediCare",
+    "production" : "high"
+}
+
+for date in hospital:
+    print(f"The hospital details are : {date} is {hospital[date]}")
